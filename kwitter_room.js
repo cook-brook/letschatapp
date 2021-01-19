d9f1395647f8811592dc0e9e531243f65b95c4e6
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-01-19 19:54:54 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBpwe30ZsOoUe4yuU-4p4M_JYHK9I5tgrQ",
+    authDomain: "kwitter-401fd.firebaseapp.com",
+    databaseURL: "https://kwitter-401fd-default-rtdb.firebaseio.com",
+    projectId: "kwitter-401fd",
+    storageBucket: "kwitter-401fd.appspot.com",
+    messagingSenderId: "592397508350",
+    appId: "1:592397508350:web:45d44d2cbeb7b6e07fcfaf"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+
